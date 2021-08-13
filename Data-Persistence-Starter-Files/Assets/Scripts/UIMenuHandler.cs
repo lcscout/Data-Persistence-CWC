affdda8e3cd0cd185d19ca68a9ec9a0cf5e5c909
coutlcdo <coutlcdo@outlook.com>
2021-08-13 16:05:29 -0300

    [WIP] Add menu and gamemanager

---
+++ Data-Persistence-Starter-Files/Assets/Scripts/UIMenuHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuHandler : MonoBehaviour {
+	public string PlayerName { get; set; }
+
+	// public void CheckName(string name) {
+	// 	Debug.Log(name);
+	// }
+}
