b79f2e1e0e8270b40ed4b083b2b49523165f128f
coutlcdo <coutlcdo@outlook.com>
2021-08-17 15:46:48 -0300

    Add data persistence between sessions

--- Data-Persistence-Starter-Files/Assets/Scripts/GameManager.cs
+++ Data-Persistence-Starter-Files/Assets/Scripts/GameManager.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
 	public static GameManager Instance;
 	public string playerName;
+
+	[System.Serializable]
+	public class HighscoreData {
+		public int _points;
+		public string _name;
+	}
+
+	public HighscoreData h_Data;
 
 	private void Awake() {
 		if (Instance != null) {
@@ -14,5 +24,25 @@ public class GameManager : MonoBehaviour {
 
 		Instance = this;
 		DontDestroyOnLoad(gameObject);
+
+		LoadHighscore();
+	}
+
+	public void SaveHighscore(int points) {
+		HighscoreData data = new HighscoreData();
+		data._points = points;
+		data._name = playerName;
+
+		string json = JsonUtility.ToJson(data);
+
+		File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+	}
+
+	public void LoadHighscore() {
+		string path = Application.persistentDataPath + "/savefile.json";
+		if (File.Exists(path)) {
+			string json = File.ReadAllText(path);
+			h_Data = JsonUtility.FromJson<HighscoreData>(json);
+		}
 	}
 }
--- Data-Persistence-Starter-Files/Assets/Scripts/MainManager.cs
+++ Data-Persistence-Starter-Files/Assets/Scripts/MainManager.cs
@@ -3,74 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
-public class MainManager : MonoBehaviour
-{
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+public class MainManager : MonoBehaviour {
+	public Brick BrickPrefab;
+	public int LineCount = 6;
+	public Rigidbody Ball;
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+	public TMP_Text highscoreText;
+	public Text ScoreText;
+	public GameObject GameOverText;
 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
-    }
+	private bool m_Started = false;
+	private int m_Points;
 
-    private void Update()
-    {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
+	private bool m_GameOver = false;
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-    }
+	private void Awake() {
+		if (GameManager.Instance != null) {
+			GameManager.HighscoreData data = GameManager.Instance.h_Data;
+			highscoreText.text = "Best Score: " + data._points.ToString() + " Name: " + data._name;
+			highscoreText.gameObject.SetActive(true);
+		}
+	}
 
-    void AddPoint(int point)
-    {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
-    }
+	void Start() {
+		const float step = 0.6f;
+		int perLine = Mathf.FloorToInt(4.0f / step);
 
-    public void GameOver()
-    {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
-    }
+		int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+		for (int i = 0; i < LineCount; ++i) {
+			for (int x = 0; x < perLine; ++x) {
+				Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+				var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+				brick.PointValue = pointCountArray[i];
+				brick.onDestroyed.AddListener(AddPoint);
+			}
+		}
+	}
+
+	private void Update() {
+		if (!m_Started) {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				m_Started = true;
+				float randomDirection = Random.Range(-1.0f, 1.0f);
+				Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+				forceDir.Normalize();
+
+				Ball.transform.SetParent(null);
+				Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+			}
+		} else if (m_GameOver) {
+			if (GameManager.Instance != null) GameManager.Instance.SaveHighscore(m_Points);
+
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+				if (GameManager.Instance != null && GameManager.Instance.h_Data != null)
+					if (m_Points > GameManager.Instance.h_Data._points)
+						GameManager.Instance.LoadHighscore();
+			}
+		}
+	}
+
+	void AddPoint(int point) {
+		m_Points += point;
+		ScoreText.text = $"Score : {m_Points}";
+	}
+
+	public void GameOver() {
+		m_GameOver = true;
+		GameOverText.SetActive(true);
+	}
 }
